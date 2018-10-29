7656036d852f48c557370521a430bc4832e48572
postlude <seungdeokhan@naver.com>
2018-10-29 21:53:48 +0900

    initial commit

---
+++ src/main/java/boot/project/wintercoding/WinterCoding2018Application.java
@@ -0,0 +1,12 @@
+package boot.project.wintercoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterCoding2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterCoding2018Application.class, args);
+	}
+}
---
+++ src/test/java/boot/project/wintercoding/WinterCoding2018ApplicationTests.java
@@ -0,0 +1,16 @@
+package boot.project.wintercoding;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WinterCoding2018ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
